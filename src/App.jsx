c16c2812f231e51772cfa0cf0723523d20a4db00
Joe Figueroa <figueroa.jose.m@gmail.com>
2018-04-17 11:02:28 -0400

    remove old

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
 //
-import {
-  CustomRoute,
-  ProtectedRoute,
-  Redirect,
-  Route,
-  Link
-} from './RouterComponents'
-//
 export class App extends React.Component {
   render() {
     return (
-      <div>
-        <CustomRoute />
-        <ProtectedRoute />
-        <Redirect />
-        <Route />
-        <Link />
+      <div> 
+      APP
       </div>
     )
   }
